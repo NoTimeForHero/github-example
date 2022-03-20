234592697a2e8b5bec59443e640d4c88eccdd31c
NoTimeForHero <notimeforhero@yandex.ru>
2022-03-20 15:42:43 +0300

    Приватные данные теперь хранятся в .env файле

--- src/services/github/settings.ts
+++ src/services/github/settings.ts
@@ -4,8 +4,8 @@ export const AuthProxyUrl = 'http://localhost:3001/';
 
 export const Settings = {
   localStorageKey: 'GitHub_OAuth',
-  client_id: '***REMOVED***',
-  client_secret: '***REMOVED***',
+  client_id: process.env.REACT_APP_CLIENT_ID,
+  client_secret: process.env.REACT_APP_CLIENT_SECRET,
   scope: ['user', 'repo'],
   redirect_uri: 'http://localhost:3000'
 }
